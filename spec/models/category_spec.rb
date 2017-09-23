940d1b30fd44868f1794f2636cd107c82201b1e9
Just Me <just806me@gmail.com>
2017-09-23 23:47:26 +0300

    fix failing spec

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,5 +3,10 @@ require 'rails_helper'
 RSpec.describe Category, type: :model do
   it { should validate_presence_of :name }
 
-  it { should validate_uniqueness_of(:name).case_insensitive }
+  context do
+    # provide a value for the name field because it's NOT NULL
+    subject { described_class.new name: 'name' }
+
+    it { should validate_uniqueness_of(:name).case_insensitive }
+  end
 end

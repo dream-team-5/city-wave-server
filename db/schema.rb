f59ebc72e862003fcbb3d23f857d4ae6c69a9ed6
Just Me <just806me@gmail.com>
2017-09-23 23:41:52 +0300

    add category

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170922085601) do
+ActiveRecord::Schema.define(version: 20170923203319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "pgcrypto"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "username"

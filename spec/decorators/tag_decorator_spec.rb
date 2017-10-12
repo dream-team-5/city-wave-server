1d3d5f043d02cb9d838f067e1aa90aef729dfe6a
Just Me <just806me@gmail.com>
2017-10-12 10:33:49 +0300

    add missing space

--- spec/decorators/tag_decorator_spec.rb
+++ spec/decorators/tag_decorator_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe TagDecorator do
   subject { described_class.new tag }
 
-  let(:tag) { stub_model Tag, name: 'name'}
+  let(:tag) { stub_model Tag, name: 'name' }
 
   describe '#as_json' do
     its(:as_json) { should eq 'id' => tag.id, 'name' => tag.name }

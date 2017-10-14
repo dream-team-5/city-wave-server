e11b0dc75bf1d968f460e2c42cd1b0ea3c061b23
Just Me <just806me@gmail.com>
2017-10-15 02:28:58 +0300

    comment sould display username instead of user_id

--- app/decorators/comment_decorator.rb
+++ app/decorators/comment_decorator.rb
@@ -2,6 +2,7 @@ class CommentDecorator < Draper::Decorator
   delegate_all
 
   def as_json options={}
-    super options.reverse_merge only: [:id, :text, :created_at, :user_id, :place_id]
+    super(options.reverse_merge only: [:id, :text, :created_at, :place_id])
+      .merge 'username' => user.username
   end
 end
--- spec/decorators/comment_decorator_spec.rb
+++ spec/decorators/comment_decorator_spec.rb
@@ -6,11 +6,17 @@ RSpec.describe CommentDecorator do
 let(:comment) { stub_model Comment, text: 'text', created_at: DateTime.now, user_id: 1, place_id: 1 }
 
   describe '#as_json' do
+    before do
+      expect(comment).to receive(:user) do
+        double.tap { |a| expect(a).to receive(:username).and_return 'username' }
+      end
+    end
+
     its(:as_json) do
       should eq \
         'id' => comment.id, 'text' => comment.text,
         'created_at' => comment.created_at,
-        'user_id' => comment.user_id,
+        'username' => 'username',
         'place_id' => comment.place_id
     end
   end

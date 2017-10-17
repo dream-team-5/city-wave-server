cd5810a8b1fcfb6793c3c2218d6d7ab00e902507
Just Me <just806me@gmail.com>
2017-10-17 21:37:49 +0300

    add path option

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -4,5 +4,6 @@ if Rails.env.production?
     dropbox_credentials: {
       access_token: ENV['DROPBOX_ACCESS_TOKEN'],
       access_type: 'app_folder'
-    }
+    },
+    path: ':id/:style/:filename'
 end

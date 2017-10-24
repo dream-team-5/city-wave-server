35aa3783056b3f740843de9c0be2277c424c2b43
Just Me <just806me@gmail.com>
2017-10-24 18:30:05 +0300

    add assets

---
+++ app/assets/javascript/application.js
@@ -0,0 +1,9 @@
+//= require jquery3
+//= require jquery_ujs
+//= require jquery-ui
+//= require popper
+//= require bootstrap
+//= require bootstrap-multiselect
+
+// = require_tree .
+// = require_self
